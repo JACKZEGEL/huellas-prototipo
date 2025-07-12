ebcf546092c0229ed52c7b41ed28bf259177fca0
JACKZEGEL <139069938+JACKZEGEL@users.noreply.github.com>
2025-07-11 19:55:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("adopcionForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("¡Gracias por tu interés en adoptar! Nos pondremos en contacto contigo pronto. 🐾");
+  this.reset();
+});
